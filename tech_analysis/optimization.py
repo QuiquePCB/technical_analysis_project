e2c8d5a4c6ac0c79cded8a103dba2800bdace7fe
QuiquePCB <quiquepcb@gmail.com>
2025-06-11 08:08:09 -0600

    Adding Strategies
    - First try to work with EWMA and Bollinger Bands

--- tech_analysis/optimization.py
+++ tech_analysis/optimization.py
@@ -34,3 +34,45 @@ class SMAOptTechAnalysis:
 
         optimal_tf_df = pd.DataFrame(train_results)
         return optimal_tf_df.sort_values(by='Return [%]', ascending=False)
+    
+    def ewma_params_n_tf_optimization(self, set_type, data, n1, n2, param = 'Return [%]'):
+        bt = Backtest(data, self.strategy, cash=10_000_000, commission=0.002)
+        stats, heatmap = bt.optimize(
+        # Óptimos parámetros a optimizar
+        n1= n1,
+        n2= n2,
+        constraint = lambda p : p.n1 < p.n2, # PREGUNTAR
+        maximize= param, 
+        return_heatmap=True # Mapa de calor
+        )
+
+        best_params = heatmap.sort_values(ascending=False)
+
+        best_results = {'interval': set_type, 'n1': best_params.index[0][0], 'n2': best_params.index[0][1], 'Return [%]': best_params.iloc[0], 'No. of Trades': stats['# Trades']}
+
+        return best_results
+    
+    def ewma_strategy_optimization(self, all_data, n1, n2):
+        train_results = []
+        for set_type, df in all_data.items():
+            if "train" in set_type:
+                train_results.append(self.ewma_params_n_tf_optimization(set_type, df, n1, n2))
+
+        optimal_tf_df = pd.DataFrame(train_results)
+        return optimal_tf_df.sort_values(by='Return [%]', ascending=False)
+    
+    def BollingerBands_params_n_tf_optimization(self, set_type, data, n1, n2, param = 'Return [%]'):
+        bt = Backtest(data, self.strategy, cash=10_000_000, commission=0.002)
+        stats, heatmap = bt.optimize(
+        # Óptimos parámetros a optimizar
+        n1= n1,
+        n2= n2,
+        maximize= param, #PREGUNTAR SOBRE LAS RESTRICCIONES
+        return_heatmap=True # Mapa de calor
+        )
+
+        best_params = heatmap.sort_values(ascending=False)
+
+        best_results = {'interval': set_type, 'n1': best_params.index[0][0], 'n2': best_params.index[0][1], 'Return [%]': best_params.iloc[0], 'No. of Trades': stats['# Trades']}
+
+        return best_results
--- tech_analysis/strategies.py
+++ tech_analysis/strategies.py
@@ -23,4 +23,39 @@ class SmaCross(Strategy):
         elif crossover(self.sma2, self.sma1):
             self.position.close()
 
-#class Startegy2(Strategy):
+class EWMA(Strategy):
+    """
+    Estrategia de medias moviles exponenciales
+    """
+    n1 = 5
+    n2 = 13
+
+    # Definir parámetros
+    def init(self):
+        self.ewma1 = self.I(EWMA, self.data.Close, self.n1)
+        self.ewma2 = self.I(EWMA, self.data.Close, self.n2)
+
+    # Definir estrategia
+    def next(self):
+        if crossover(self.ewma1, self.ewma2):
+            self.buy()
+        elif crossover(self.ewma2, self.ewma1):
+            self.position.close()
+
+class BollingerBands(Strategy):
+    """
+    Estrategia de bandas de Bollinger
+    """
+    n1 = 20
+    n2 = 2
+
+    # Definir parámetros
+    def init(self):
+        self.bb = self.I(BollingerBands, self.data.Close, self.n1, self.n2)
+
+    # Definir estrategia
+    def next(self):
+        if self.data.Close[-1] < self.bb.lowerband[-1]:
+            self.buy()
+        elif self.data.Close[-1] > self.bb.upperband[-1]:
+            self.position.close()
