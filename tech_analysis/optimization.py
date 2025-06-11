abf6241a22ef3b0dd685c26d5b052a08abda55a2
Carolina-Martinez12 <carolina.martinez@iteso.mx>
2025-06-11 08:39:51 -0600

    Report and Optimization

--- tech_analysis/optimization.py
+++ tech_analysis/optimization.py
@@ -35,6 +35,11 @@ class SMAOptTechAnalysis:
         optimal_tf_df = pd.DataFrame(train_results)
         return optimal_tf_df.sort_values(by='Return [%]', ascending=False)
     
+class EWMAOptTechAnalysis:
+    def __init__(self, Strategy):
+        self.strategy = Strategy
+        pass
+    
     def ewma_params_n_tf_optimization(self, set_type, data, n1, n2, param = 'Return [%]'):
         bt = Backtest(data, self.strategy, cash=10_000_000, commission=0.002)
         stats, heatmap = bt.optimize(
@@ -61,18 +66,37 @@ class SMAOptTechAnalysis:
         optimal_tf_df = pd.DataFrame(train_results)
         return optimal_tf_df.sort_values(by='Return [%]', ascending=False)
     
-    def BollingerBands_params_n_tf_optimization(self, set_type, data, n1, n2, param = 'Return [%]'):
-        bt = Backtest(data, self.strategy, cash=10_000_000, commission=0.002)
+class BollingerBandsOptTechAnalysis:
+    def __init__(self, Strategy):
+        self.strategy = Strategy
+        pass
+    
+    def BollingerBands_params_n_tf_optimization(self, set_type, data,
+                                                n1_range, n2_range,
+                                                param='Return [%]'):
+        bt = Backtest(data, self.strategy,
+                      cash=10_000_000, commission=0.002)
         stats, heatmap = bt.optimize(
-        # Óptimos parámetros a optimizar
-        n1= n1,
-        n2= n2,
-        maximize= param, #PREGUNTAR SOBRE LAS RESTRICCIONES
-        return_heatmap=True # Mapa de calor
+            n1 = n1_range,
+            n2 = n2_range,
+            constraint = lambda p: (p.n1 > 1) and (p.n2 > 0),
+            maximize = param,
+            return_heatmap = True,
         )
 
-        best_params = heatmap.sort_values(ascending=False)
-
+        best_params = heatmap.sort_values(ascending=False).iloc[0]
+        
         best_results = {'interval': set_type, 'n1': best_params.index[0][0], 'n2': best_params.index[0][1], 'Return [%]': best_params.iloc[0], 'No. of Trades': stats['# Trades']}
 
         return best_results
+
+    def bollinger_strategy_optimization(self, all_data, n1_range, n2_range):
+        results = []
+        for set_type, df in all_data.items():
+            if "train" in set_type:
+                res = self.BollingerBands_params_n_tf_optimization(
+                    set_type, df, n1_range, n2_range
+                )
+                results.append(res)
+        df_opt = pd.DataFrame(results)
+        return df_opt.sort_values(by='Return [%]', ascending=False)
